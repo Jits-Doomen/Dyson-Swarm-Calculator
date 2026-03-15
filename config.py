26c217edac14fd13a43566e61f6ed04830f0a27b
Jits Doomen <jits.doomen@outlook.com>
2026-03-15 19:36:14 +0100

    Fix comment formatting in PhysicalConstants class

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ class PhysicalConstants:
     M = 1.989e30
     G = 6.674e-11
     SIGMA = 5.670e-8
-    SGP = G * M //Standard Gravitational Parameter (μ)
+    SGP = G * M
+    ## SGP = Standard Gravitational Parameter (μ)
